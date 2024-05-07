7fd2847567d7cd712a9ca7a5580a6624ac311d89
Aaron Stubblefield <aaron.g.stubblefield@dartmouth.edu>
2024-05-07 11:33:15 -0400

    more plotting script edits

--- source/wrapper.py
+++ source/wrapper.py
@@ -4,12 +4,11 @@ from mpi4py import MPI
 from params import nz
 import numpy as np
 
-z_0 = np.arange(3.9,12.1,0.1)  
-timesteps = np.linspace(0,1e3,1000)
 
-
-def wrapper(i,v_i):
+def wrapper_v(i,v_i):
     N_f = 2.0  # effective stress at base of fringe 
+    timesteps = np.linspace(0,1e3,1000)
+    z_0 = np.arange(3.9,12.1,0.1)  
     z_l0 = z_0[i]
     z_b = 1e-3
     gamma = 0
@@ -20,3 +19,16 @@ def wrapper(i,v_i):
     N, heave, visc, z, new_lens, converged = time_stepping(domain,initial,N_f,N_c,gamma,const_phi,v_i,timesteps,eps=1e-10)
     z_l = z[:,-1]
     return([z_l,new_lens,[converged]])
+
+
+def wrapper_N(i,N_f,v_i,timesteps):
+    z_l = 5
+    z_b = 1e-3
+    domain = create_interval(MPI.COMM_WORLD,nz,[z_b,z_l])
+    gamma = 0
+    const_phi = False
+    N_c = 0.05
+    initial = initialize(domain,N_f,gamma,const_phi,eps_min=1e-10)
+    N, heave, visc, z, new_lens, converged = time_stepping(domain,initial,N_f,N_c,gamma,const_phi,v_i[i],timesteps,eps=1e-10)
+    z_l = z[:,-1]
+    return([z_l,new_lens])
